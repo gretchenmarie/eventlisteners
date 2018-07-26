3bed754a0da0b2891f1c1ec90b3f814f2a95a519
gretchenmarie <gretchenmward@gmail.com>
2018-07-26 15:06:23 -0500

    added activate mindreading

--- JS/powers.js
+++ JS/powers.js
@@ -19,14 +19,32 @@ document.querySelector("#activate-flight").addEventListener("click", function(){
 });
 
 
-/*
-    Now write two more event handlers that activate the other two powers
+
+   /* Now write two more event handlers that activate the other two powers
     when the corresponding buttons are clicked.
 */
+/*document.getElementById("activate-mindreading").innerHTML = 'activated';*/
+var powerUp2 = document.getElementById ("powerList")
+
+document.querySelector("#activate-mindreading").addEventListener("click", function(){
+    {
+        powerUp2.classList.add("enabled")
+        powerUp2.classList.remove("disabled")
+        
+        console.log("powerUp2.classList", powerUp2.classList)
+    };
+});
 
 
-/*
-    Write two more event handlers for activating and deactivating all powers
+/*document.querySelector("#mindreading").addEventListener("click", function(){
+    {
+        powerUp.classList.add(" power enabled")
+        powerUp.classList.remove(" power disabled")
+        
+        console.log("powerUp.classList", powerUp.classList)
+      };
+});
+  /*  Write two more event handlers for activating and deactivating all powers
     when the corresponding buttons are clicked. You will need to use the
     `document.querySelectorAll()` method for these.
 */
